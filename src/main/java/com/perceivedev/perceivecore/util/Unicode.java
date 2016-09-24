d93d228af700677baac3500554dc498998f81846
Rayzr522 <rayzr522@gmail.com>
2016-09-24 09:28:34 -0400

    Made Unicode an enum
    Just for you, I Al Istannen :P

--- src/main/java/com/perceivedev/perceivecore/util/Unicode.java
+++ src/main/java/com/perceivedev/perceivecore/util/Unicode.java
@@ -1,56 +1,72 @@
 
 package com.perceivedev.perceivecore.util;
 
-public class Unicode {
+public enum Unicode {
 
-    public static final String X                    = "\u2716";
-    public static final String X_THIN               = "\u2715";
-    public static final String CHECK                = "\u2714";
-    public static final String CHECK_THIN           = "\u2713";
+    X("\u2716"),
+    X_THIN("\u2715"),
+    CHECK("\u2714"),
+    CHECK_THIN("\u2713"),
 
-    public static final String CROSS                = "\u271e";
-    public static final String COFFEE               = "\u2615";
-    public static final String HEART                = "\u2764";
-    public static final String FEMALE               = "\u2640";
-    public static final String MALE                 = "\u2642";
-    public static final String INFINITY             = "\u221e";
-    public static final String COLON_WHITE          = "\u2982";
+    CROSS("\u271e"),
+    COFFEE("\u2615"),
+    HEART("\u2764"),
+    FEMALE("\u2640"),
+    MALE("\u2642"),
+    INFINITY("\u221e"),
+    COLON_WHITE("\u2982"),
 
-    public static final String PENCIL_LOWER_RIGHT   = "\u270e";
-    public static final String PENCIL_RIGHT         = "\u270f";
-    public static final String PENCIL_UPPER_RIGHT   = "\u2710";
+    PENCIL_LOWER_RIGHT("\u270e"),
+    PENCIL_RIGHT("\u270f"),
+    PENCIL_UPPER_RIGHT("\u2710"),
 
-    public static final String CIRCLE_SMALL         = "\u2022";
+    CIRCLE_SMALL("\u2022"),
 
-    public static final String BLOCK_THIN           = "\u2758";
-    public static final String BLOCK_MEDIUM         = "\u2759";
-    public static final String BLOCK_THICK          = "\u275a";
-    public static final String BLOCK_FULL           = "\u2588";
+    BLOCK_THIN("\u2758"),
+    BLOCK_MEDIUM("\u2759"),
+    BLOCK_THICK("\u275a"),
+    BLOCK_FULL("\u2588"),
 
-    public static final String DOUBLE_ANGLE_RIGHT   = "\u00bb";
-    public static final String DOUBLE_ANGLE_LEFT    = "\u00Ab";
+    DOUBLE_ANGLE_RIGHT("\u00bb"),
+    DOUBLE_ANGLE_LEFT("\u00Ab"),
 
-    public static final String TRIANGLE_BLACK_UP    = "\u25b2";
-    public static final String TRIANGLE_WHITE_UP    = "\u25b3";
-    public static final String TRIANGLE_BLACK_RIGHT = "\u25b6";
-    public static final String TRIANGLE_WHITE_RIGHT = "\u25b7";
-    public static final String TRIANGLE_BLACK_DOWN  = "\u25bc";
-    public static final String TRIANGLE_WHITE_DOWN  = "\u25bd";
-    public static final String TRIANGLE_BLACK_LEFT  = "\u25c0";
-    public static final String TRIANGLE_WHITE_LEFT  = "\u25c1";
+    TRIANGLE_BLACK_UP("\u25b2"),
+    TRIANGLE_WHITE_UP("\u25b3"),
+    TRIANGLE_BLACK_RIGHT("\u25b6"),
+    TRIANGLE_WHITE_RIGHT("\u25b7"),
+    TRIANGLE_BLACK_DOWN("\u25bc"),
+    TRIANGLE_WHITE_DOWN("\u25bd"),
+    TRIANGLE_BLACK_LEFT("\u25c0"),
+    TRIANGLE_WHITE_LEFT("\u25c1"),
 
-    public static final String STAR_4               = "\u2726";
-    public static final String STAR_WHITE_4         = "\u2727";
-    public static final String STAR_6               = "\u2736";
-    public static final String STAR_8               = "\u2738";
+    STAR_4("\u2726"),
+    STAR_WHITE_4("\u2727"),
+    STAR_6("\u2736"),
+    STAR_8("\u2738"),
 
-    public static final String ARROW_TRIPLE_LINE    = "\u279f";
-    public static final String ARROW_ROUND          = "\u279c";
-    public static final String ARROW_CIRCLE         = "\u27b2";
+    ARROW_TRIPLE_LINE("\u279f"),
+    ARROW_ROUND("\u279c"),
+    ARROW_CIRCLE("\u27b2"),
 
-    public static final String ARROW_SMALL_LEFT     = "\u2190";
-    public static final String ARROW_SMALL_RIGHT    = "\u2192";
-    public static final String ARROW_WHITE_LEFT     = "\u21e6";
-    public static final String ARROW_WHITE_RIGHT    = "\u21e8";
+    ARROW_SMALL_LEFT("\u2190"),
+    ARROW_SMALL_RIGHT("\u2192"),
+    ARROW_WHITE_LEFT("\u21e6"),
+    ARROW_WHITE_RIGHT("\u21e8");
+
+    private String character;
+
+    Unicode(String character) {
+        this.character = character;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Enum#toString()
+     */
+    @Override
+    public String toString() {
+        return character;
+    }
 
 }
