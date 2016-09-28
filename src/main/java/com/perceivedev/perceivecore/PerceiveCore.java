824115be86cbdbe141e3aef8ae28d7e82e28f909
Rayzr522 <blood314@icloud.com>
2016-09-28 12:47:39 -0400

    Revert "Update PerceiveCore.java"

--- src/main/java/com/perceivedev/perceivecore/PerceiveCore.java
+++ src/main/java/com/perceivedev/perceivecore/PerceiveCore.java
@@ -4,9 +4,5 @@ package com.perceivedev.perceivecore;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PerceiveCore extends JavaPlugin {
-  
-  public void onEnable() {
-      getLogger().log(Level.INFO, "*** PerceiveCore has been enabled on your server. ***");
-  }
 
 }
