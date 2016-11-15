5f8e85930ea979fc7dfb8d1e786ec2da406c754b
Rayzr522 <rayzr522@gmail.com>
2016-11-15 17:12:51 -0500

    Added DataFileManager

--- src/main/java/com/perceivedev/perceivecore/config/util/DataFileManager.java
+++ src/main/java/com/perceivedev/perceivecore/config/util/DataFileManager.java
@@ -3,13 +3,8 @@ package com.perceivedev.perceivecore.config.util;
 import static com.perceivedev.perceivecore.util.TextUtils.normalizePathName;
 
 import java.io.IOException;
-import java.nio.file.FileVisitOption;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
@@ -22,39 +17,39 @@ import com.perceivedev.perceivecore.config.ConfigSerializable;
 import com.perceivedev.perceivecore.config.SerializationManager;
 
 /**
- * A {@link DataManager} that saves data in one file per key of the map in the
- * specified folder
+ * A {@link DataManager} that saves all data in one file, organized by sections
+ * that the key as their name
  */
-public class DataFolderManager<K, V extends ConfigSerializable> extends DataManager<K, V> {
+public class DataFileManager<K, V extends ConfigSerializable> extends DataManager<K, V> {
 
     /**
-     * Creates a new {@link DataFolderManager} that saves and loads data
+     * Creates a new {@link DataFileManager} that saves and loads data
      * class of the
      * type specified, and stores them in the given map.
      *
-     * @param path The path to the data folder
+     * @param path The path to the data file
      * @param keyClass The class for the key.
      *            {@link SerializationManager#isSerializableToString(Class)}
      *            must return true when given this.
      * @param dataClass The data class that this {@link DataManager} handles
      * @param map The map to store the data in
      */
-    public DataFolderManager(Path path, Class<K> keyClass, Class<V> dataClass, Map<K, V> map) {
+    public DataFileManager(Path path, Class<K> keyClass, Class<V> dataClass, Map<K, V> map) {
         super(path, keyClass, dataClass, map);
     }
 
     /**
-     * Creates a new {@link DataFolderManager} that uses a
+     * Creates a new {@link DataFileManager} that uses a
      * {@link HashMap}
      *
-     * @param path The path to the data folder
+     * @param path The path to the data file
      * @param keyClass The class for the key.
      *            {@link SerializationManager#isSerializableToString(Class)}
      *            must return true when given this.
      * @param dataClass The data class that this {@link DataManager} handles
-     * @see #DataFolderManager(Path, Class, Class, Map)
+     * @see #DataFileManager(Path, Class, Class, Map)
      */
-    public DataFolderManager(Path path, Class<K> keyClass, Class<V> dataClass) {
+    public DataFileManager(Path path, Class<K> keyClass, Class<V> dataClass) {
         this(path, keyClass, dataClass, new HashMap<>());
     }
 
@@ -62,14 +57,14 @@ public class DataFolderManager<K, V extends ConfigSerializable> extends DataMana
      * Creates a new {@link DataManager}
      *
      * @param plugin The plugin to get the Data folder from
-     * @param path The path to the data folder
+     * @param path The path to the data file
      * @param keyClass The class for the key.
      *            {@link SerializationManager#isSerializableToString(Class)}
      *            must return true when given this.
      * @param dataClass The data class that this {@link DataManager} handles
-     * @see #DataFolderManager(Path, Class, Class)
+     * @see #DataFileManager(Path, Class, Class)
      */
-    public DataFolderManager(Plugin plugin, String path, Class<K> keyClass, Class<V> dataClass) {
+    public DataFileManager(Plugin plugin, String path, Class<K> keyClass, Class<V> dataClass) {
         this(plugin.getDataFolder().toPath().resolve(normalizePathName(path)), keyClass, dataClass);
     }
 
@@ -80,9 +75,9 @@ public class DataFolderManager<K, V extends ConfigSerializable> extends DataMana
 
     @Override
     public void save() {
-        for (Map.Entry<K, V> entry : map.entrySet()) {
-            YamlConfiguration configuration = new YamlConfiguration();
+        YamlConfiguration configuration = new YamlConfiguration();
 
+        for (Map.Entry<K, V> entry : map.entrySet()) {
             Object serializedKey = SerializationManager.serializeOneLevel(entry.getKey());
             if (!(serializedKey instanceof String)) {
                 PerceiveCore.getInstance().getLogger().log(Level.WARNING, "Fascinating."
@@ -91,39 +86,30 @@ public class DataFolderManager<K, V extends ConfigSerializable> extends DataMana
                         + " Value: " + entry.getKey());
                 continue;
             }
+
             Map<String, Object> serializedValue = SerializationManager.serialize(entry.getValue());
 
             configuration.createSection((String) serializedKey, serializedValue);
+        }
 
-            try {
-                configuration.save(getPath().resolve((String) serializedKey + ".yml").toFile());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        try {
+            configuration.save(getPath().toFile());
+        } catch (IOException e) {
+            PerceiveCore.getInstance().getLogger().log(Level.WARNING, "DataFileManager failed to save it's data to the disk! "
+                    + "This is most likely not the fault of PerceiveCore.", e);
         }
     }
 
     @Override
     public void load() {
-        try {
-            Files.walkFileTree(path, EnumSet.noneOf(FileVisitOption.class), 0, new SimpleFileVisitor<Path>() {
-                @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                    YamlConfiguration configuration = YamlConfiguration.loadConfiguration(file.toFile());
+        YamlConfiguration configuration = YamlConfiguration.loadConfiguration(path.toFile());
 
-                    for (String configKey : configuration.getKeys(false)) {
-                        V value = SerializationManager.deserialize(dataClass, configuration.getConfigurationSection(configKey));
-                        @SuppressWarnings("unchecked")
-                        K key = (K) SerializationManager.deserializeOneLevel(configKey, keyClass);
+        for (String configKey : configuration.getKeys(false)) {
+            V value = SerializationManager.deserialize(dataClass, configuration.getConfigurationSection(configKey));
+            @SuppressWarnings("unchecked")
+            K key = (K) SerializationManager.deserializeOneLevel(configKey, keyClass);
 
-                        put(key, value);
-                    }
-                    return FileVisitResult.CONTINUE;
-                }
-            });
-        } catch (IOException e) {
-            PerceiveCore.getInstance().getLogger().log(Level.WARNING, "Failed to read a File in DataFolderManager. "
-                    + "This is most likely not the fault of PerceiveCore.", e);
+            put(key, value);
         }
     }
 }
